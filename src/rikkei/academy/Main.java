86aa8351149f55620fe8b81b0012d5f6d8d564ee
h110401 <hung110401@gmail.com>
2022-08-01 15:09:18 +0700

    th2

--- src/rikkei/academy/Main.java
+++ src/rikkei/academy/Main.java
@@ -1,6 +1,7 @@
 package rikkei.academy;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
@@ -48,19 +49,19 @@ public class Main {
 
         ComparableCircle[] circles = new ComparableCircle[3];
         circles[0] = new ComparableCircle(3.6);
-        circles[1] = new ComparableCircle();
+        circles[1] = new ComparableCircle(3.5,"black", true);
         circles[2] = new ComparableCircle(3.5, "indigo", false);
 
-        System.out.println("Pre-sorted: ");
-        for (ComparableCircle comparableCircle : circles) {
-            System.out.println(comparableCircle);
+        System.out.println("Pre-sorted:");
+        for (ComparableCircle circle1 : circles) {
+            System.out.println(circle1);
         }
 
         Arrays.sort(circles);
 
         System.out.println("After-sorted: ");
-        for (ComparableCircle comparableCircle : circles) {
-            System.out.println(comparableCircle);
+        for (ComparableCircle circle1 : circles) {
+            System.out.println(circle1);
         }
 
     }
